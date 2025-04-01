1719e8912db89aef6c1e4f0a95788e438a90b952
KurtAstarita <itskurtastarita@gmail.com>
2025-04-01 17:45:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-  const exercises = {
+function sanitizeInput(element) {
+    element.value = element.value.replace(/[<>]/g, "");
+}
+
+document.getElementById('paste-text').addEventListener('input', function() {
+    sanitizeInput(this);
+});
+
+const exercises = {
     muscle: {
         beginner: {
             dumbbells: [
