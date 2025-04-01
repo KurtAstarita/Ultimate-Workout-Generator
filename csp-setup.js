0d95803e1b8b9f11cf22d84c8d6b5655c692ad90
KurtAstarita <itskurtastarita@gmail.com>
2025-04-01 04:41:52 -0400

    Update csp-setup.js

--- csp-setup.js
+++ csp-setup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const meta = document.createElement('meta');
     meta.httpEquiv = "Content-Security-Policy";
-    meta.content = "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com https://raw.githubusercontent.com; style-src 'self' https://www.blogger.com; img-src 'self' https://blogger.googleusercontent.com https://www.blogger.com; font-src 'self'; frame-src 'self' https://www.blogger.com https://www.blogger.com/comment/frame/ https://www.blogger.com/navbar/; connect-src 'self' https://www.google.com;";
+    meta.content = "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com https://raw.githubusercontent.com; style-src 'self' https://www.blogger.com; img-src 'self' https://blogger.googleusercontent.com https://www.blogger.com; font-src 'self'; frame-src 'self' https://www.blogger.com https://www.blogger.com/comment/frame/ https://www.blogger.com/navbar/; connect-src 'self' https://www.google.com; report-uri /csp-report;"; // Add report-uri
     document.head.appendChild(meta);
 });
