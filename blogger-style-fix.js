2fb9e6df5f0d32abf16e9d3fe46374c7d9a60a8c
KurtAstarita <itskurtastarita@gmail.com>
2025-04-01 05:23:48 -0400

    Delete blogger-style-fix.js

--- blogger-style-fix.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const linkElement = document.querySelector('link[href*="authorization.css"]');
-    if (linkElement && linkElement.media !== 'all') {
-        linkElement.media = 'all';
-    }
-});
