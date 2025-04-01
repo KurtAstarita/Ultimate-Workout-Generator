1ccb95e18b11054d9ebd5373c0fd32cb3894774b
KurtAstarita <itskurtastarita@gmail.com>
2025-04-01 05:25:05 -0400

    Update csp-setup.js

--- csp-setup.js
+++ csp-setup.js
@@ -1,6 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const meta = document.createElement('meta');
-    meta.httpEquiv = "Content-Security-Policy";
-    meta.content = "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com https://raw.githubusercontent.com; style-src 'self' https://www.blogger.com; img-src 'self' https://blogger.googleusercontent.com https://www.blogger.com; font-src 'self'; frame-src 'self' https://www.blogger.com https://www.blogger.com/comment/frame/ https://www.blogger.com/navbar/; connect-src 'self' https://www.google.com; report-uri /csp-report;"; // Add report-uri
-    document.head.appendChild(meta);
-});
+<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline';">
