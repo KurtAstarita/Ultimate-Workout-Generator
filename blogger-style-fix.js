ca0757b92e3ab94f56daa008fc911da296d206c1
KurtAstarita <itskurtastarita@gmail.com>
2025-04-01 00:41:11 -0400

    Update blogger-style-fix.js

--- blogger-style-fix.js
+++ blogger-style-fix.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const meta = document.createElement('meta');
-    meta.httpEquiv = "Content-Security-Policy";
-    meta.content = "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com https://raw.githubusercontent.com; style-src 'self' https://www.blogger.com; img-src 'self' https://blogger.googleusercontent.com; font-src 'self'; frame-src 'self' https://www.blogger.com https://www.blogger.com/comment/frame/;";
-    document.head.appendChild(meta);
+document.addEventListener('DOMContentLoaded', function() {
+    const linkElement = document.querySelector('link[href*="authorization.css"]');
+    if (linkElement && linkElement.media !== 'all') {
+        linkElement.media = 'all';
+    }
 });
